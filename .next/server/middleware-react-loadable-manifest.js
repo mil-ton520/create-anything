a846dae331bd12bec708642f8f5cf184e5ca1ea1
mil-ton520 <miltonlyconet@gmail.com>
2025-10-16 21:00:53 +0200

    chore: add .gitignore e deixar de trackear node_modules/.next

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"components/HistoryPanel.tsx -> @/components/charts/HistoryArea\":{\"id\":\"components/HistoryPanel.tsx -> @/components/charts/HistoryArea\",\"files\":[\"static/chunks/_app-pages-browser_components_charts_HistoryArea_tsx.js\"]}}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"UyKcTIaETIa+Yq6AE26cm21zqMU/1PPggMwAeLVuoCM=\"\n}"
